11ca4fc42dd3f1f58610f01965cdc8d9e130cdda
SePo <sebastianpopa@live.com>
2023-06-27 13:08:09 +0300

    made the themoviedb API working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react"
+import React, { useEffect, useState } from "react";
+import requests from "../Requests"
+import axios from "axios"
+
+
 
 const Main = () => {
     const [movies, setMovies] = useState([])
 
+    useEffect(() => {
+      const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+      
+
+      axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {   //request popular
+            headers: {
+              accept: 'application/json',
+              Authorization: accessToken
+            },
+      })
+          .then(response => {
+            setMovies(response.data.results);
+          })
+          .catch(error => {
+            console.error(error);
+          });   
+  }, []);
+  console.log(movies);
+      
   return (
     <div>Main</div>
   )
