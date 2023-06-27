77ad43d56f7c600a65b26805b65500f6120b8b40
SePo <sebastianpopa@live.com>
2023-06-27 14:03:43 +0300

    added context

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,34 +1,64 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios"
-import requests from "../Requests";
-
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import requests from '../Requests';
 
 const Main = () => {
-    const [movies, setMovies] = useState([])
+   const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
+   const movie = movies[Math.floor(Math.random() * movies.length)];
+
+   useEffect(() => {
       const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
-      
 
-      axios.get(requests.requestPopular, {   
+      axios
+         .get(requests.requestPopular, {
             headers: {
-              accept: 'application/json',
-              Authorization: accessToken
+               accept: 'application/json',
+               Authorization: accessToken,
             },
-      })
-          .then(response => {
+         })
+         .then(response => {
             setMovies(response.data.results);
-          })
-          .catch(error => {
+         })
+         .catch(error => {
             console.error(error);
-          });   
-  }, []);
-  console.log(movies);
-      
-  return (
-    <div>Main</div>
-  )
-}
+         });
+   }, []);
+  //  console.log(movie);
 
-export default Main
+  const truncateString = (str, num) => {
+    if(str?.length>num) {
+      return str.slice(0,num) + '...'
+    } else {
+      return str;
+    }
+  };
+
+   return (
+      <div className='w-full h-[550px] text-white'>
+         <div className='w-full h-full'>
+            <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+            <img
+               className='w-full h-full object-cover'
+               src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+               alt={movie?.title}
+            />
+            <div className='absolute w-full top-[20%] p-4 md:p-8'>
+              <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+               <div>
+                  <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
+                     Play
+                  </button>
+                  <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+                     Watch Later
+                  </button>
+               </div>
+               <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
+               <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncateString(movie?.overview, 200)}</p>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Main;
