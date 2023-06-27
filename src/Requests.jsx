2a2ea404ad0700529d4bd8b9f03acea8f4d4e308
SePo <sebastianpopa@live.com>
2023-06-27 10:45:13 +0300

    added API key

---
+++ src/Requests.jsx
@@ -0,0 +1,13 @@
+
+
+const key =import.meta.env.VITE_APP_KEY;
+
+const requests = {
+    requestPopular: ``,
+    requestTopRated: ``,
+    requestTrending: ``,
+    requestHorror: ``,
+    requestUpcoming: ``,
+};
+
+export default requests
