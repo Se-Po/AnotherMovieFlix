6acec58852d35143b69d1363fdb2bf9c8b096475
SePo <sebastianpopa@live.com>
2023-06-28 17:05:05 +0300

    added horizontal scroll

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,45 +1,70 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import Movie from './Movie';
+
+const Row = ({ title, fetchURL }) => {
+   const [movies, setMovies] = useState([]);
+   const [like, setLike] = useState(false);
+   const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+   useEffect(() => {
+      const options = {
+         method: 'GET',
+         url: fetchURL,
+         params: { language: 'en-US', page: '1' },
+         headers: {
+            accept: 'application/json',
+            Authorization: accessToken,
+         },
+      };
+
+      axios
+         .request(options)
+         .then(response => {
+            setMovies(response.data.results);
+         })
+         .catch(function (error) {
+            console.error(error);
+         });
+   }, [fetchURL]);
 
 
 
-const Row = ({title,fetchURL}) => {
-const [movies, setMovies] = useState([])
-const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+   return (
+      <>
+         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+         <div className='relative flex items-center group'>
+          <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+         <div id={"slider"} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+               {movies.map((item, id) => (
+                  <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                     <img
+                        className='w-full h-auto block'
+                        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+                        alt={item?.title}
+                     />
+                     <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                        <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                           {item.title}
+                        </p>
+                        <p>
+                           {like ? (
+                              <FaHeart className='absolute top-4 left-4 text-gray-300' />
+                           ) : (
+                              <FaRegHeart className='absolute top-4 left-4 text-gray-300' />
+                           )}
+                        </p>
+                     </div>
+                  </div>
+                  // <Movie />
+               ))}
+            </div>
+            <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+         </div>
+      </>
+   );
+};
 
-  useEffect(() =>{
-    const options = {
-      method: 'GET',
-      url: fetchURL,
-      params: {language: 'en-US', page: '1'},
-      headers: {
-        accept: 'application/json',
-        Authorization: accessToken,
-      }
-    };
-
-    axios
-    .request(options)
-    .then(response => {
-     setMovies(response.data.results);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-}, [fetchURL]);
-
-  console.log(movies)
-
-  return (
-    <>
-    <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-    <div className='relative flex items-center'>
-      <div id={"slider"}>
-
-      </div>
-    </div>
-    </>
-  )
-}
-
-export default Row
+export default Row;
