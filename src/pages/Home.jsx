be0333e8893236d5d8403a555d55922cedb4da00
SePo <sebastianpopa@live.com>
2024-01-05 15:40:10 +0200

    typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Main />
       <Row rowID='1' title='Upcoming' fetchURL={requests.requestUpcoming}/>
       <Row rowID='2' title='Popular' fetchURL={requests.requestPopular}/>
-      <Row rowID='3' title='TopRated' fetchURL={requests.requestTopRated}/>
+      <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated}/>
     </>
   )
 }
